5a97032a775cd21b0c05ecd06e995a566fef071e
bill fumerola <bill@andyl.com>
2025-11-20 11:00:49 -0800

    feat: set up protocol muxing for incremental migration
    - Add tf5to6server to upgrade SDK v2 provider to protocol 6
    - Add tf6muxserver to combine SDK v2 and Framework providers
    - Update main.go to serve muxed provider
    - SDK v2 provider serves all existing resources
    - Framework provider starts empty
    
    All resources currently served by SDK v2 provider.
    Resources will be migrated incrementally to Framework provider.
    
    Provider builds and all tests pass.

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.24.6
 
 require (
 	github.com/hashicorp/go-cty v1.5.0
-	github.com/hashicorp/go-hclog v1.6.3
 	github.com/hashicorp/terraform-plugin-framework v1.16.1
+	github.com/hashicorp/terraform-plugin-go v0.29.0
+	github.com/hashicorp/terraform-plugin-mux v0.21.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.38.1
 	github.com/netactuate/gona v0.0.0-20240411214507-62f71253081f
 )
@@ -17,12 +18,12 @@ require (
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-hclog v1.6.3 // indirect
 	github.com/hashicorp/go-plugin v1.7.0 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
 	github.com/hashicorp/go-version v1.7.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.24.0 // indirect
 	github.com/hashicorp/logutils v1.0.0 // indirect
-	github.com/hashicorp/terraform-plugin-go v0.29.0 // indirect
 	github.com/hashicorp/terraform-plugin-log v0.10.0 // indirect
 	github.com/hashicorp/terraform-registry-address v0.4.0 // indirect
 	github.com/hashicorp/terraform-svchost v0.1.1 // indirect
--- main.go
+++ main.go
@@ -1,36 +1,69 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 
-	"github.com/hashicorp/go-hclog"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
+
 	"github.com/netactuate/terraform-provider-netactuate/netactuate"
 )
 
 const (
 	ProviderAddr = "registry.terraform.io/netactuate/netactuate"
+	version      = "dev"
 )
 
 func main() {
-	var debugMode bool
-
-	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	var debug bool
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers")
 	flag.Parse()
 
-	opts := &plugin.ServeOpts{
-		ProviderFunc: netactuate.Provider,
-		ProviderAddr: ProviderAddr,
+	ctx := context.Background()
 
-		Debug: debugMode,
-		Logger: hclog.FromStandardLogger(
-			log.Default(),
-			&hclog.LoggerOptions{
-				Name: "terraform-provider-netactuate",
-			},
-		),
+	// Wrap the SDK v2 provider with tf5to6server to upgrade it to protocol 6
+	upgradedSdkProvider, err := tf5to6server.UpgradeServer(
+		ctx,
+		netactuate.NewSDKProvider(version).GRPCProvider,
+	)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	plugin.Serve(opts)
+	// Create the new Plugin Framework provider
+	frameworkProvider := providerserver.NewProtocol6(
+		netactuate.NewFrameworkProvider(version),
+	)
+
+	// Mux the providers together
+	providers := []func() tfprotov6.ProviderServer{
+		func() tfprotov6.ProviderServer {
+			return upgradedSdkProvider
+		},
+		frameworkProvider,
+	}
+
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, providers...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serveOpts []tf6server.ServeOpt
+	if debug {
+		serveOpts = append(serveOpts, tf6server.WithManagedDebug())
+	}
+
+	err = tf6server.Serve(
+		ProviderAddr,
+		muxServer.ProviderServer,
+		serveOpts...,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
